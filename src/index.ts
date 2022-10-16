0f137d0283e94715b9cc94b1f67e7f3f94d9356e
launchpencil <66775280+launchpencil@users.noreply.github.com>
2022-10-16 14:13:21 +0900

    できた

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import request = require("request");
+var TOKEN: string = process.env.LINE_ACCESS_TOKEN;
+var sendurl: string;
+
+interface getObj {
+  uri: string;
+  method: string;
+  headers: {
+    [key: string]: string;
+  };
+  json: boolean;
+}
+interface postObj {
+  uri: string;
+  headers: {
+    [key: string]: string;
+  };
+  json: {
+    [key: string]: string;
+  };
+}
+interface msg {
+  "type": String;
+  text: null | string;
+  packageId: null | string;
+  stickerId: null | string;
+  originalContentUrl: null | string;
+  previewImageUrl: null | string;
+}
+interface source {
+  "type": String;
+  "userId": String;
+}
+interface delcon {
+  "isRedelivery": boolean;
+}
+interface events {
+  "type": String;
+  "message": null | msg;
+  "timestamp": Number;
+  "source": source;
+  "replyToken": null | String;
+  "mode": String;
+  "webhookEventId": String;
+  "deliveryContext": delcon;
+}
+
+export function setup(url: string) {
+    sendurl = url;
+}
+
+export function senddiscord(content: events) {
+    var options: getObj = {
+      uri: "https://api.line.me/v2/bot/profile/" + content.source.userId,
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + TOKEN
+      },
+      json: true
+    };
+    request(options, function (error, response, body) {
+      console.log(body);
+      var sendmsg: postObj = {
+        uri: sendurl,
+        headers: {
+          "Content-type": "application/json"
+        },
+        json: {
+          "username": body.displayName,
+          "avatar_url": body.pictureUrl,
+          "content": content.message.text
+        }
+      }
+      request.post(sendmsg);
+    });
+  }
